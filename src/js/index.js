69912ccf7f260a08865d3f4ace3452f8ce5111f5
kanedev <kanedev@live.fr>
2019-07-15 17:55:29 +0200

    work in progress

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,10 @@
 
 
 // // include through webpack.ProvidePlugin with exports-loader
-import 'bootstrap/js/dist/util'
- import 'bootstrap/js/dist/carousel'
- import 'bootstrap/js/dist/dropdown'
- import 'bootstrap/js/dist/tab'
+// import 'bootstrap/js/dist/util'
+//  import 'bootstrap/js/dist/carousel'
+//  import 'bootstrap/js/dist/dropdown'
+//  import 'bootstrap/js/dist/tab'
 
 // import "exports-loader?Alert!bootstrap/js/dist/alert"
 // import "exports-loader?Button!bootstrap/js/dist/button"
@@ -16,11 +16,11 @@ import 'bootstrap/js/dist/util'
 // import "exports-loader?Modal!bootstrap/js/dist/modal"
 // import "exports-loader?Popover!bootstrap/js/dist/popover"
 // import "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy"
- import "exports-loader?Tab!bootstrap/js/dist/tab"
- import "exports-loader?Tooltip!bootstrap/js/dist/tooltip"
+//  import "exports-loader?Tab!bootstrap/js/dist/tab"
+//  import "exports-loader?Tooltip!bootstrap/js/dist/tooltip"
 
 //import 'bootstrap/js/dist/modal'
-import 'bootstrap/js/dist/tooltip'
+// import 'bootstrap/js/dist/tooltip'
 
 console.log('Getting ready...');
 
@@ -28,3 +28,9 @@ $(function() {
   console.log('Ready!');
 });
 
+jQuery(document).ready(function() {
+	$('.carousel').carousel({
+    	pause: "false",
+    	interval: 7000
+	});
+   }); 

7fd04e44b955fe4c692d9489b436faf6c3eebf47
kanedev <kanedev@live.fr>
2019-11-09 08:29:32 +0100

    ✨ feat : show modal image

--- src/js/index.js
+++ src/js/index.js
@@ -37,6 +37,24 @@ jQuery(document).ready(function() {
 	})
 });
 
+ 
+    $(".openPhotoModal").click(function(){
+		var srcImg = $(this).data("whatever");
+        $("#photoWorkModal img").attr("src",srcImg);
+        $("#photoWorkModal").modal("show");
+    })
+ 
+
+// $('#photoWorkModal').on('show.bs.modal', function (event) {
+// 	var button = $(event.relatedTarget) // Button that triggered the modal
+// 	var recipient = button.data('whatever') // Extract info from data-* attributes
+// 	// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+// 	// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+// 	var modal = $(this)
+// 	//modal.find('.modal-title').text('New message to ' + recipient)
+// 	//modal.find('.modal-body input').val(recipient)
+// 	modal.find('.modal-body img').setAttribute('src',recipient)
+//   })
 
 // var ctx = $("#myChart");
 // var ctx2= $("#myChart2");
